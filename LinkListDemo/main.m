55262c218aa0f5c2057421899c0b986b33e77d10
lsandro <42097314@qq.com>
2016-10-10 14:56:42 +0800

    Initial Commit

---
+++ LinkListDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinkListDemo
+//
+//  Created by YI on 16/10/10.
+//  Copyright © 2016年 Sandro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
